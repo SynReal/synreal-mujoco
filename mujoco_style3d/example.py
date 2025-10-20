63ded0f25de7e2390090c68dd90ed23ac971a956
zeagle <zeagle01@163.com>
2025-10-20 15:57:31 +0800

    feat: handy helpers that craete piece easily from xml

--- mujoco_style3d/example.py
+++ mujoco_style3d/example.py
@@ -1,77 +1,22 @@
 import time
 
 import style3dsim as sim
-import numpy as np
 
-import mujoco
 import mujoco.viewer
 
-import mj_to_sim
-
-def log_callback(file_name: str, func_name: str, line: int, level: sim.LogLevel, message: str):
-	if level == sim.LogLevel.INFO:
-		print("[info]: ", message)
-	elif level == sim.LogLevel.ERROR:
-		print("[error]: ", message)
-	elif level == sim.LogLevel.WARNING:
-		print("[warning]: ", message)
-	elif level == sim.LogLevel.DEBUG:
-		print("[debug]: ", message)
-
-
-def load_data(xml_path):
-    m = mujoco.MjModel.from_xml_path(xml_path)
-    d = mujoco.MjData(m)
-    mujoco.mj_forward(m, d)  # so that d is populated by m
-    return m,d
-
-
-def add_piece_to_sim(x,t,name):
-
-    cloth = sim.Cloth(t, x, np.array([], dtype=float), False)
-
-    cloth_attrib = sim.ClothAttrib()
-    cloth_attrib.stretch_stiff = sim.Vec3f(120, 100, 80)
-    cloth_attrib.bend_stiff = sim.Vec3f(1e-6, 1e-6, 1e-6)
-    cloth_attrib.density = 0.2
-    cloth_attrib.static_friction = 0.03
-    cloth_attrib.dynamic_friction = 0.03
-
-    cloth.set_attrib(cloth_attrib)
-
-    global world
-
-    cloth.attach(world)
-
-    sim_pieces.append(cloth)
-    piece_names.append(name)
+import helper
 
 def log_in(usr,pw):
     sim.login(usr, pw, True, None)
 
-
-def sim_init():
-    sim.set_log_callback(log_callback)
-
-    world = sim.World()
-    world_attrib = sim.WorldAttrib()
-    world_attrib.enable_gpu = True
-    world_attrib.gravity = sim.Vec3f(0, 0, -9.81)
-    world_attrib.time_step = 0.01
-    world.set_attrib(world_attrib)
-    return world
-
-
 log_in('simsdk001','xSXiaCMd')
 
 #'F:/mujoco_proj/piper_description/mujoco_model/piper_bimanual_description_act_tmp.xml'
-m,d=load_data('F:/mujoco_proj/4_grid/scene.xml')
+m,d = helper.load_data('F:/mujoco_proj/4_grid/scene.xml')
 
-world=sim_init()
+world = helper.get_a_sim_world()
 
-sim_pieces=[]
-piece_names=[]
-mj_to_sim.for_each_piece(m, d, add_piece_to_sim)
+sim_pieces, piece_names = helper.add_piece_to_sim(m,d,world)
 
 with mujoco.viewer.launch_passive(m, d) as viewer:
     while True:
@@ -84,9 +29,9 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
         duration1 = end1_t - begin1_t
 
         world.fetch_sim(0)
-        for cloth,cloth_name in zip(sim_pieces,piece_names):
-            x = cloth.get_positions()
-            mj_to_sim.set_positions(m, d, cloth_name, x)
+
+        helper.set_piece_to_mujoco(m, d, sim_pieces, piece_names)
+
         viewer.sync()
 
         end0_t = time.time()
---
+++ mujoco_style3d/helper.py
@@ -0,0 +1,65 @@
+import style3dsim as sim
+import mujoco
+import numpy as np
+
+import mj_to_sim
+
+
+def _add_piece_to_sim(x,t,name,world,sim_pieces,piece_names):
+
+    cloth = sim.Cloth(t, x, np.array([], dtype=float), False)
+
+    cloth_attrib = sim.ClothAttrib()
+    cloth_attrib.stretch_stiff = sim.Vec3f(120, 100, 80)
+    cloth_attrib.bend_stiff = sim.Vec3f(1e-6, 1e-6, 1e-6)
+    cloth_attrib.density = 0.2
+    cloth_attrib.static_friction = 0.03
+    cloth_attrib.dynamic_friction = 0.03
+
+    cloth.set_attrib(cloth_attrib)
+
+    cloth.attach(world)
+
+    sim_pieces.append(cloth)
+    piece_names.append(name)
+
+
+def _log_callback(file_name: str, func_name: str, line: int, level: sim.LogLevel, message: str):
+    if level == sim.LogLevel.INFO:
+        print("[info]: ", message)
+    elif level == sim.LogLevel.ERROR:
+        print("[error]: ", message)
+    elif level == sim.LogLevel.WARNING:
+        print("[warning]: ", message)
+    elif level == sim.LogLevel.DEBUG:
+        print("[debug]: ", message)
+
+def get_a_sim_world():
+    sim.set_log_callback(_log_callback)
+
+    world = sim.World()
+    world_attrib = sim.WorldAttrib()
+    world_attrib.enable_gpu = True
+    world_attrib.gravity = sim.Vec3f(0, 0, -9.81)
+    world_attrib.time_step = 0.01
+    world.set_attrib(world_attrib)
+    return world
+
+
+def load_data(xml_path):
+    m = mujoco.MjModel.from_xml_path(xml_path)
+    d = mujoco.MjData(m)
+    mujoco.mj_forward(m, d)  # so that d is populated by m
+    return m,d
+
+def add_piece_to_sim(m,d,world):
+    sim_pieces = []
+    piece_names = []
+    add_piece = lambda x,t,name :_add_piece_to_sim(x,t,name,world,sim_pieces,piece_names)
+    mj_to_sim.for_each_piece(m, d, add_piece)
+    return sim_pieces,piece_names
+
+def set_piece_to_mujoco(m,d,sim_pieces,piece_names):
+    for cloth, cloth_name in zip(sim_pieces, piece_names):
+        x = cloth.get_positions()
+        mj_to_sim.set_positions(m, d, cloth_name, x)
